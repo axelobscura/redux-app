e18159585aef02b344525623ce8cd672f711b4ad
Axel Obscura <axosar@gmail.com>
2020-04-03 10:53:18 -0600

    Adding skeleton

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const DashboardPage = () => (
   <section>
-    <h1>Dashboard</h1>
+    <h1>Dashboard OF POSTS</h1>
     <p>This is the dashboard.</p>
 
     <Link to="/posts" className="button">
